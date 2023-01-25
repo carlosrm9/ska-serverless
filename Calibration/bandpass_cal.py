ba94b27f6c37cf1a14260c2c8709123c759cbde9
carlosrm9 <113986518+carlosrm9@users.noreply.github.com>
2023-01-25 15:57:38 +0100

    Update bandpass_cal.py

--- Calibration/bandpass_cal.py
+++ Calibration/bandpass_cal.py
@@ -1,7 +1,7 @@
 import casatasks as ct
 import casaplotms as csplot
 
-ct.bandpass(vis='ngc5921.demo.ms', caltable='ngc5921.demo.bcal', field='0', selectdata=False,bandtype='B', solint='inf', combine='scan', refant='15')
+ct.bandpass(vis='/data/ngc5921.demo.ms', caltable='/data/ngc5921.demo.bcal', field='0', selectdata=False,bandtype='B', solint='inf', combine='scan', refant='15')
 
 # csplot.plotms(vis='ngc5921.demo.bcal', field='0', gridrows=2, gridcols=1, plotindex=0, rowindex=0, 
 #       xaxis='channel', yaxis='amp', showgui=False, clearplots=False, coloraxis='antenna1',
