fe78e27012b7b842c4cc44d347f31f0a7c60565b
carlosrm9 <113986518+carlosrm9@users.noreply.github.com>
2023-01-25 15:58:41 +0100

    Update fluxscale.py

--- Calibration/fluxscale.py
+++ Calibration/fluxscale.py
@@ -1,3 +1,3 @@
 import casatasks as ct
 
-ct.fluxscale(vis='ngc5921.demo.ms', fluxtable='ngc5921.demo.fluxscale', caltable='ngc5921.demo.gcal', reference='1331*', transfer='1445*')
+ct.fluxscale(vis='/data/ngc5921.demo.ms', fluxtable='/data/ngc5921.demo.fluxscale', caltable='ngc5921.demo.gcal', reference='1331*', transfer='1445*')
