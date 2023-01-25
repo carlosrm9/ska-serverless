df28f9e5cf6eb52626b25e3617afd3ebda826dd6
carlosrm9 <113986518+carlosrm9@users.noreply.github.com>
2023-01-25 16:04:16 +0100

    Update uvcontsub.py

--- Calibration/uvcontsub.py
+++ Calibration/uvcontsub.py
@@ -1,6 +1,6 @@
 import casatasks as ct
 
-ct.split(vis='ngc5921.demo.ms', outputvis='ngc5921.demo.src.split.ms', field='N5921*', spw='', datacolumn='corrected')
+ct.split(vis='/data/ngc5921.demo.ms', outputvis='/data/ngc5921.demo.src.split.ms', field='N5921*', spw='', datacolumn='corrected')
 
-ct.uvcontsub(vis='ngc5921.demo.src.split.ms', field='N5921*', fitspw='0:4~6;50~59', spw='0', solint=0.0, fitorder=0, 
+ct.uvcontsub(vis='/data/ngc5921.demo.src.split.ms', field='N5921*', fitspw='0:4~6;50~59', spw='0', solint=0.0, fitorder=0, 
         want_cont=True)
