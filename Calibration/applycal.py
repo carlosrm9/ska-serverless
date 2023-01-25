8fa1d361867cdbfa2cb0b9bb3a0012a2f4a96731
carlosrm9 <113986518+carlosrm9@users.noreply.github.com>
2023-01-25 15:56:56 +0100

    Update applycal.py

--- Calibration/applycal.py
+++ Calibration/applycal.py
@@ -1,7 +1,7 @@
 import casatasks as ct
 
-ct.applycal(vis='ngc5921.demo.ms', field='1,2', gaintable=['ngc5921.demo.fluxscale','ngc5921.demo.bcal'], 
+ct.applycal(vis='/data/ngc5921.demo.ms', field='1,2', gaintable=['/data/ngc5921.demo.fluxscale','/data/ngc5921.demo.bcal'], 
          gainfield=['1','*'], interp=['linear','nearest'], spwmap=[], selectdata=False)
-ct.applycal(vis='ngc5921.demo.ms', field='0', gaintable=['ngc5921.demo.fluxscale','ngc5921.demo.bcal'], gainfield=['0','*'], 
+ct.applycal(vis='/data/ngc5921.demo.ms', field='0', gaintable=['/data/ngc5921.demo.fluxscale','/data/ngc5921.demo.bcal'], gainfield=['0','*'], 
          interp=['linear','nearest'], spwmap=[], selectdata=False)
 
