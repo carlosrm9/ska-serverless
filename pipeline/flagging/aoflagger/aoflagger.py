7553778e87999fb7f6ca9d60e7ae50ad81d981d2
carlosrm9 <riosmonjecarlos@gmail.com>
2023-02-02 12:12:49 +0100

    Aoflagger Files

---
+++ pipeline/flagging/aoflagger/aoflagger.py
@@ -0,0 +1,28 @@
+import stimela
+import os
+
+INPUT = 'input/' # Input directory  (It must exit)
+OUTPUT = 'output/' # Output directory
+MSDIR = 'msdir/' # Measurement Set directory
+PREFIX = 'stimela-example' # Prefix for output images
+
+MSNAME = 'meerkat_simulation_example.ms' # Measurent Set Name
+
+pipeline = stimela.Recipe('Flagging with Aoflagger',
+			  ms_dir=MSDIR,
+			  indir=INPUT,
+			  outdir=OUTPUT,
+			  log_dir=os.path.join(OUTPUT,'logs'),
+			  )
+with open("parameters.txt", "r") as f:
+    parameters = f.read()
+
+params = eval(parameters)
+
+pipeline.add("cab/autoflagger",	# this is the full name of the executor image
+    "flagging_aoflagger",		# container label
+    params, 	# These are the options set above
+    label="Flag data")	# Task label. For logging purposes
+
+ 
+pipeline.run()
