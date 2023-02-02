0bb6328104cf0997bdc40d341c76a52575e45d44
carlosrm9 <riosmonjecarlos@gmail.com>
2023-02-02 15:12:22 +0100

    tclean parametrization

---
+++ pipeline/cleaning/casa-6.5.0/tcleanparam.py
@@ -0,0 +1,14 @@
+import casatasks as ct
+import os
+
+with open("parameters2.txt", "r") as file:
+    parameters = file.read()
+
+params = eval(parameters)
+ct.tclean(**params)
+
+path = '.'
+cleans = [clean for clean in os.listdir(path) if clean.startswith('ngc5921.demo.tclean') ]
+
+for clean in cleans:
+        ct.exportfits(imagename=clean,fitsimage=clean+'.fits',overwrite=True)
