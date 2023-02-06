30a4025d77edf3c02c72c65526dc64746cc207da
carlosrm9 <riosmonjecarlos@gmail.com>
2023-02-06 13:26:57 +0100

    json parameters

--- pipeline/calibration/gaincal/casa-6.5.0/gaincal.py
+++ pipeline/calibration/gaincal/casa-6.5.0/gaincal.py
@@ -1,8 +1,12 @@
+import sys
+import json
 import casatasks as ct
 
-with open("parameters.txt", "r") as file:
-    parameters = file.read()
+parameters=sys.argv[1]
 
-params = eval(parameters)
+with open(parameters, "r") as file:
+    param_json = file.read()
+
+params = json.loads(param_json)
 
 ct.gaincal(**params)
