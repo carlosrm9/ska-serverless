ea6282439804ad9a2d79c91bcb361b58785358d6
carlosrm9 <riosmonjecarlos@gmail.com>
2023-02-09 12:41:23 +0100

    aoflagger docker version

---
+++ pipeline/flagging/aoflagger-2.13.0/script.py
@@ -0,0 +1,20 @@
+import sys
+import json
+import subprocess
+
+parameters = sys.argv[1]
+
+params = json.loads(parameters)
+
+result = []
+for key, value in params.items():
+    if key == "ms":
+        continue
+    if type(value) == list:
+        result.append(f"-{key}")
+        for item in value:
+            result.append(str(item))
+    else:
+        result.append(f"-{key} {value}")
+parameters_str=" ".join(result)
+subprocess.run(["aoflagger"] + parameters_str.split() + [params["ms"]])
