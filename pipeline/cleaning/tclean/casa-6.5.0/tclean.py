e01f03128ef160c74157e55962bee30f22df9a63
carlosrm9 <riosmonjecarlos@gmail.com>
2023-02-17 12:18:27 +0100

    Global parameters

--- pipeline/cleaning/tclean/casa-6.5.0/tclean.py
+++ pipeline/cleaning/tclean/casa-6.5.0/tclean.py
@@ -3,11 +3,29 @@ import json
 import casatasks as ct
 import os,sys
 
-param_json=sys.argv[1]
+parameters = sys.argv[1]
+translator = sys.argv[2]
 
-params = json.loads(param_json)
+params = json.loads(parameters)
+trans = json.loads(translator)
 
-ct.tclean(**params)
+trans_params = {}
+
+trans_params = {}
+for k, v in params.items():
+    if v is not None:
+        trans_k = trans.get(k, k)
+        if trans_k is None:
+            continue
+        elif trans_k == "savemodel":
+            if v:
+                trans_params[trans_k] = "modelcolumn"
+            else: 
+                trans_params[trans_k] = "none"
+        else:
+            trans_params[trans_k] = v
+
+ct.tclean(**trans_params)
 
 path = '.'
 cleans = [clean for clean in os.listdir(path) if clean.startswith('ngc5921.demo.tclean') ]
