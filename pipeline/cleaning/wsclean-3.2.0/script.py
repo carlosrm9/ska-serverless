a26f24a8321e5f4b12e55995041850201a28b623
carlosrm9 <riosmonjecarlos@gmail.com>
2023-02-17 11:05:09 +0100

    Global parametrization

--- pipeline/cleaning/wsclean-3.2.0/script.py
+++ pipeline/cleaning/wsclean-3.2.0/script.py
@@ -3,18 +3,33 @@ import json
 import subprocess
 
 parameters = sys.argv[1]
+translator = sys.argv[2]
 
 params = json.loads(parameters)
+trans = json.loads(translator)
+
+trans_params = updated_dict = {trans.get(k, k): v for k, v in params.items()}
 
 result = []
-for key, value in params.items():
+for key, value in trans_params.items():
     if key == "MS-Name":
         continue
-    if type(value) == list:
+    elif key == "update-model":
+        if value:
+            result.append(f"-{key}"+"-required")
+        else:
+            result.append("-no"+f"-{key}"+"-required")
+    elif key == "reorder":
+        if value:
+            result.append(f"-{key}")
+        else:
+            result.append("-no"+f"-{key}")
+
+    elif type(value) == list:
         result.append(f"-{key}")
         for item in value:
             result.append(str(item))
     else:
         result.append(f"-{key} {value}")
 parameters_str=" ".join(result)
-subprocess.run(["wsclean"] + parameters_str.split() + [params["MS-Name"]])
+subprocess.run(["wsclean"] + parameters_str.split() + [trans_params["MS-Name"]])
