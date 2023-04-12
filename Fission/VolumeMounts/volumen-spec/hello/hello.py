3175af4052d771a75a3d7b37a64be535e6700ee7
Carlos RM <riosmonjecarlos@gmail.com>
2023-04-12 09:59:28 +0000

    Volume Mounting

--- Fission/VolumeMounts/volumen-spec/hello/hello.py
+++ Fission/VolumeMounts/volumen-spec/hello/hello.py
@@ -1,6 +1,6 @@
 import os
 
 def main():
-    file_list = os.listdir('/data')
+    file_list = os.listdir('/mnt')
     file_list_str = "\n".join(file_list)
     return file_list_str
--- Fission/volumen-spec/hello/hello.py
+++
@@ -1,2 +0,0 @@
-def main():
-    return "Hello, world!\nThis is a test!\n"
