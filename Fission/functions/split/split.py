f4209c737a0a576844526a89bbbdb05bc833f490
Carlos RM <riosmonjecarlos@gmail.com>
2023-04-18 14:40:19 +0000

    Casa Python Pipeline

--- Fission/functions/split/split.py
+++ Fission/functions/split/split.py
@@ -6,13 +6,12 @@ import casatasks as ct
 import subprocess
 
 def main():
-    # Get tclean parameters as the json body of a POST request:
     param = request.get_json()
     input = param["Input-MS"]
     output = param["Output-MS"]
-    del param["Input-MS"] 
+    del param["Input-MS"]
     del param["Output-MS"]
     inpath = '/app/data/' + input
     outpath = '/app/data/' + output
-    ct.tclean(vis = inpath, imagename= outpath, **param)
-    return 'Done!'
+    ct.split(vis = inpath, outputvis = outpath, **param)
+    return 'Done!\n'
